3de34afbec31bb60a763b7168897beed96aab426
DoHuy <dohuy171@gmail.com>
2020-01-06 15:26:34 +0700

    define new models

--- model/model.go
+++ model/model.go
@@ -5,9 +5,7 @@ import (
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
-type ErrorMessage struct {
-	Message		string		`json:"error_message"`
-}
+
 
 type UserDevice struct {
 	ID				int 		`gorm:"column:id" json:"id,omitempty"`
@@ -32,36 +30,35 @@ type Credential struct {
 
 type Owner struct {
 	CredentialId		int			`gorm:"column:credentialId" json:"credentialId,omitempty"`
-	Credential			Credential	`json:"credential,omitempty"`
 	FullName			string		`gorm:"column:fullName"  json:"fullName,omitempty"`
 	PhoneNumber			string		`gorm:"column:phoneNumber" json:"phoneNumber,omitempty"`
 	Address				string		`gorm:"column:address" json:"address,omitempty"`
 	CMNDImage			string		`gorm:"column:cmndImage" json:"cmndImage,omitempty"`
 	Status				string		`gorm:"column:status" json:"status,omitempty"`
 	Parkings			[]Parking	`json:"parkings, omitempty"`
-	ParkingId			int			`gorm:"column:parkingId" json:"parkingId,omitempty"`
 	CreatedAt			string		`gorm:"column:created_at" json:"created_at,omitempty"`
 	ModifiedAt			string		`gorm:"column:modified_at" json:"modified_at,omitempty"`
 }
 
 type Parking struct {
-	ID					int			`gorm:"column:id" json:"id, omitempty"`
-	ParkingName			string		`gorm:"column:parkingName" json:"parkingName, omitempty"`
-	Properties			string		`gorm:"column:properties" json:"properties, omitempty"`
-	Address				string		`gorm:"column:address" json:"address, omitempty"`
-	KindOf				bool		`gorm:"column:kindOf" json:"kindOf, omitempty"`
-	ParkingImages 		string		`gorm:"column:parkingImages" json:"parkingImages, omitempty"`
-	Payment				string		`gorm:"column:payment" json:"payment, omitempty"`
-	Longitude			string		`gorm:"column:longitude" json:"longitude, omitempty"`
-	Latitude			string		`gorm:"column:latitude" json:"latitude, omitempty"`
-	Capacity			string		`gorm:"column:capacity" json:"capacity, omitempty"`
-	BlockAmount			int			`gorm:"column:blockAmount" json:"blockAmount, omitempty"`
-	CertificateOfLand	string		`gorm:"column:certificateOfLand" json:"certificateOFLand, omitempty"`
-	CreatedAt			string		`gorm:"column:created_at" json:"created_at, omitempty"`
-	ModifiedAt 			string		`gorm:"column:modified_at" json:"modified_at, omitempty"`
-	DeletedAt 			string		`gorm:"column:deleted_at" json:"deleted_at, omitempty"`
-	Describe			string		`gorm:"column:describe" json:"describe, omitempty"`
-	Status				string		`gorm:"column:status" json:"status, omitempty"`
+	ID					int			`gorm:"column:id" json:"id,omitempty"`
+	ParkingName			string		`gorm:"column:parkingName" json:"parkingName,omitempty"`
+	Properties			string		`gorm:"column:properties" json:"properties,omitempty"`
+	Address				string		`gorm:"column:address" json:"address,omitempty"`
+	KindOf				string		`gorm:"column:kindOf" json:"kindOf,omitempty"`
+	ParkingImages 		string		`gorm:"column:parkingImages" json:"parkingImages,omitempty"`
+	Payment				string		`gorm:"column:payment" json:"payment,omitempty"`
+	Longitude			string		`gorm:"column:longitude" json:"longitude,omitempty"`
+	Latitude			string		`gorm:"column:latitude" json:"latitude,omitempty"`
+	Capacity			string		`gorm:"column:capacity" json:"capacity,omitempty"`
+	BlockAmount			int			`gorm:"column:blockAmount" json:"blockAmount,omitempty"`
+	OwnerId				int			`gorm:"column:ownerId" json:"ownerId,omitempty"`
+	CertificateOfLand	string		`gorm:"column:certificateOfland" json:"certificateOfland,omitempty"`
+	CreatedAt			string		`gorm:"column:created_at" json:"created_at,omitempty"`
+	ModifiedAt 			string		`gorm:"column:modified_at" json:"modified_at,omitempty"`
+	DeletedAt 			string		`gorm:"column:deleted_at" json:"deleted_at,omitempty"`
+	Describe			string		`gorm:"column:describe" json:"describe,omitempty"`
+	Status				string		`gorm:"column:status" json:"status,omitempty"`
 }
 
 type Transaction struct {
@@ -77,9 +74,10 @@ type Transaction struct {
 	Status			string			`gorm:"column:status" json:"status,omitempty"`
 	ReasonMsg		string			`gorm:"column:reasonMsg" json:"reasonMsg,omitempty"`
 	CreatedAt		string			`gorm:"column:created_at" json:"created_at,omitempty"`
-	ModifiedAt		string			`gorm:"column:modified_at" json:"modified_at,omitempty"`
+	ModifiedAt		string			`gorm:"column:modified_at" json:"modified_at"`
 
 }
+
 
 type Payload struct {
 	UserId  int     `json:"userId"`
